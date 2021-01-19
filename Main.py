9bb4e4045054063e38a5afcb6b8a37a8bebf62af
gihanjayatilaka <gihanjayatilaka@eng.pdn.ac.lk>
2021-01-19 15:56:22 +0530

    update

--- Main.py
+++ Main.py
@@ -1,5 +1,28 @@
 from scholarly import scholarly
 import argparse
+from time import sleep
+import random
+
+
+DELAY_TIME_SEC=1.0
+
+def delay():
+	sleep(random.uniform(1.0*DELAY_TIME_SEC, 2.0*DELAY_TIME_SEC))
+
+
+def pub2String(publication,titleOnly=True):
+	if titleOnly:
+		return "=={}",format(publication['bib']['title'])
+	else:
+		ans=[]
+		toAdd=['author','title','journal','publisher','pub_year']
+		for t in toAdd:
+			try:
+				ans.append(publication['bib'][t])
+			except:
+				_=""
+		ans=". ".join(ans)
+		return ans
 
 if __name__=="__main__":
 	
@@ -7,8 +30,12 @@ if __name__=="__main__":
 	args=argparse.ArgumentParser()
 	args.add_argument("--name",type=str,dest="name",default=None)
 	args.add_argument("--year",type=int,dest="year",default=None)
+	args.add_argument("--allDetails",type=bool,dest="allDetails",default=False)
+	args.add_argument("--delaySec",type=float,dest="delaySec",default=1.0)
 
 	args=args.parse_args()
+
+	DELAY_TIME_SEC=args.delaySec
 	
 	authors=scholarly.search_author(args.name)
 	author=next(authors)
@@ -35,24 +62,27 @@ if __name__=="__main__":
 
 	for pub in author['publications']:
 		# print(pub)
+		delay()
 		scholarly.fill(pub)
 		# print(pub)
 		try:
 			thisPubCitationIdx=1
-			print("{}\t {}\t {}.\t{}".format(pubIdx,"-","-",pub['bib']['title']))
-			
+			print("{}\t {}\t {}.\t{}".format(pubIdx,"-","-",pub2String(pub,titleOnly=False)))
+			delay()
 			citations=scholarly.citedby(pub)
 			for citation in citations:
 				if citation['bib']['pub_year']==str(args.year):
-					print("{}\t {}\t {}.\t{}".format(pubIdx,thisPubCitationIdx,allPubsCitationIdx,citation['bib']['title']))
+					if args.allDetails==False:
+						print("{}\t {}\t {}.\t{}".format(pubIdx,thisPubCitationIdx,allPubsCitationIdx,pub2String(citation)))
+					else:
+						print("{}\t {}\t {}.\t{}".format(pubIdx,thisPubCitationIdx,allPubsCitationIdx,pub2String(citation,titleOnly=False)))
 					thisPubCitationIdx+=1
 					allPubsCitationIdx+=1
-			pubIdx+=1
-
 		except:
 				print("err")
 				errors.append(pub['bib']['title'])
 
+		pubIdx+=1
 
 
 	if len(errors)>0:
--- README.md
+++ README.md
@@ -8,9 +8,16 @@ pip install scholarly
 
 
 ## Running
+
+Basic
 ```
 python Main.py --name "Alan Turing" --year 2020
+```
 
+Advanced
+Basic
+```
+python Main.py --name "Alan Turing" --year 2020 --allDetails True --delaySec 4
 ```
 
 ## Issues
